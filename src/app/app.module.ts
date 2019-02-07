19ec87f136cd0cf32e48fa0e5ad2152cbd3e2fa1
Blagis <julianbgs96@gmail.com>
2019-02-07 22:47:45 +0300

    new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {environment} from '../environments/environment';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+import {AuthService} from './auth/auth.service';
+
+import {AppComponent} from './app.component';
+import {UserLoginComponent} from './auth/user-login/user-login.component';
+import {UserInfoComponent} from './auth/user-info/user-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserLoginComponent,
+    UserInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
