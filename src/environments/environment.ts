ee09fed20c94bc31e7a509ace79d9feca15f0b3d
Julian <julianbgs96@gmail.com>
2019-02-08 10:43:02 +0300

    change project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: 'AIzaSyAGj-IgiaRUmsIZnF-aPGm7Me00l_Kd8VM',
-        authDomain: 'angular-firebase-7e7d4.firebaseapp.com',
-        databaseURL: 'https://angular-firebase-7e7d4.firebaseio.com',
-        projectId: 'angular-firebase-7e7d4',
-        storageBucket: 'angular-firebase-7e7d4.appspot.com',
-        messagingSenderId: '171840495816'
-    }
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAGj-IgiaRUmsIZnF-aPGm7Me00l_Kd8VM',
+    authDomain: 'angular-firebase-7e7d4.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-7e7d4.firebaseio.com',
+    projectId: 'angular-firebase-7e7d4',
+    storageBucket: 'angular-firebase-7e7d4.appspot.com',
+    messagingSenderId: '171840495816'
+  }
 };
